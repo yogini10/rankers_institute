64617bbf6806c1744032f4a12ca179597dc11e8f
Garmode3073 <gauravgarmode16@gmail.com>
2021-01-15 07:17:30 +0530

    documentation and arrangement

--- lib/screens/frontPage.dart
+++ lib/screens/frontPage.dart
@@ -21,6 +21,7 @@ class FrontPage extends StatelessWidget {
               allowDrawingOutsideViewBox: true,
             ),
           ),
+          //Login button box
           Transform.translate(
             offset: Offset(width * 1 / 5, 527.0),
             child: Container(
@@ -39,6 +40,7 @@ class FrontPage extends StatelessWidget {
               ),
             ),
           ),
+          //Login button text
           Transform.translate(
             offset: Offset(width * 1 / 3, 530.0),
             child: PageLink(
@@ -61,6 +63,7 @@ class FrontPage extends StatelessWidget {
               ),
             ),
           ),
+          // background image
           Transform.translate(
             offset: Offset(0.0, 125.0),
             child:
@@ -76,6 +79,7 @@ class FrontPage extends StatelessWidget {
               ),
             ),
           ),
+          //Logo
           Transform.translate(
             offset: Offset(width * 1.5 / 5, 86.0),
             child:
@@ -97,5 +101,6 @@ class FrontPage extends StatelessWidget {
   }
 }
 
+//round shape for background
 const String _svg_e8meo3 =
     '<svg viewBox="-162.0 2.0 691.0 798.0" ><path transform="translate(-162.0, 2.0)" d="M 345.5 0 C 536.3143920898438 0 691 178.6384124755859 691 399 C 691 619.361572265625 536.3143920898438 798 345.5 798 C 154.6856231689453 798 0 619.361572265625 0 399 C 0 178.6384124755859 154.6856231689453 0 345.5 0 Z" fill="#90e0ef" fill-opacity="0.47" stroke="none" stroke-width="1" stroke-opacity="0.47" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
